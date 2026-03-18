aa720738ff71dc1ef6052e59ea358e9d06235876
areebmohd <areebmohd683@gmail.com>
2026-03-18 17:55:15 +0530

    added .env

--- src/services/supabaseClient.ts
+++ src/services/supabaseClient.ts
@@ -1,7 +1,5 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = REDACTED_URL;
-const SUPABASE_ANON_KEY = REDACTED_KEY;
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
